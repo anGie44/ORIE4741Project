731980018acd8c34d1767c71b06b0b918d0defde
Angie Pinilla <angelinepinilla@gmail.com>
2016-10-28 06:42:37 -0400

    added randomforest

--- data-processing/cleanData.py
+++ data-processing/cleanData.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import Imputer
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import LinearSVC
-
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
 
 def importAndClean():
     df = pd.read_csv("HCMST_train.csv", header=0, low_memory=False)
@@ -28,18 +28,23 @@ def importAndClean():
 
     return x_train, y_train, x_test, y_test
 
-def applyClassifier(x,y,x_test):
+def applyClassifier(type,x,y,x_test):
     # Fit and Predict on New Data
     # In this case, predicting on training data b/c test_data has different dimensions for now...
-    x_train_pred = OneVsRestClassifier(LinearSVC(random_state=0)).fit(x, y).predict(x)
-    
-    return x_train_pred
+    if type == 'multiclass':
+        pred = OneVsRestClassifier(LinearSVC(random_state=0)).fit(x, y).predict(x)
+    elif type == 'forest':
+        pred = RandomForestClassifier(n_estimators=10, max_depth=None, random_state=0).fit(x,y).predict(x)
+
+    return pred
 
 def main():
     x_train, y_train, x_test, y_test = importAndClean()
-    pred = applyClassifier(x_train, y_train, x_test)
+    pred_mc = applyClassifier('multiclass', x_train, y_train, x_test)
+    pred_rf = applyClassifier('forest', x_train, y_train, x_test)
     
-    print("Classifier Accuracy: %.2f" % np.mean(y_train == pred))
+    print("Classifier Accuracy: %.2f" % np.mean(y_train == pred_mc))
+    print("Classifier Accuracy: %.2f" % np.mean(y_train == pred_rf))
 
 if __name__ == "__main__":
     main()
